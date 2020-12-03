f9a2b3e99253ba68eb9acec455badab9433a14a7
Jaehyun Song <tintstyle@gmail.com>
2020-12-03 16:01:36 +0900

    Add files via upload

--- SimpleConjoint.js
+++ SimpleConjoint.js
@@ -4,6 +4,7 @@ var Tasks = 1;
 var Profiles = 2;
 // 先頭文字
 var Letter = "F";
+
 // 属性名配列
 var KeyArray = ["A", "B", "C"];
 // 水準配列
@@ -12,6 +13,7 @@ var FeaturesArray = {"A":["A1", "A2", "A3"],
                      "C":["C1", "C2", "C3", "C4"]};
 
 // Fisher-Yatesシャッフル
+// なんでJavaScriptには配列シャフル関数がないんだ
 function shuffle(array){
 	for (var i = array.length  - 1; i > 0; i--) {
 		var j = Math.floor(Math.random () * (i + 1));
@@ -30,15 +32,17 @@ function choose_one(array) {
 	return chosen_element;
 };
 
+// 属性順番のランダム化
+// 順番を固定する場合、コメントアウト
 var KeyArray = shuffle(KeyArray);
 
 for (i = 1; i <= Tasks; i++) {
 	for (j = 0; j <= KeyArray.length - 1; j++) {
 		Qualtrics.SurveyEngine.setEmbeddedData(Letter + "-" + i + "-" + (j+1), KeyArray[j]);
-		//console.log(Letter + "-" + i + "-" + (j+1) + ":" + KeyArray[j]); // for Debugging
+		//console.log(Letter + "-" + i + "-" + (j+1) + ":" + KeyArray[j]); // デバッグ用
 		for (k = 1; k <= Profiles; k++) {
 			Qualtrics.SurveyEngine.setEmbeddedData(Letter + "-" + i + "-" + k + "-" + (j+1), choose_one(FeaturesArray[KeyArray[j]]));
-			//console.log(Letter + "-" + i + "-" + (j+1) + "-" + k + ":" + choose_one(FeaturesArray[KeyArray[j]]));  // for Debugging
+			//console.log(Letter + "-" + i + "-" + (j+1) + "-" + k + ":" + choose_one(FeaturesArray[KeyArray[j]]));  // デバッグ用
 		}
 	}
 }
